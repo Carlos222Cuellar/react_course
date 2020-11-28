b2c23bc40616b38dfc1e101d648cbe1700e3234a
Carlos Cuellar <carlos12cuellar@outlook.com>
2020-11-28 10:04:50 -0600

    usando Hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'; //importamos react
 import '../assets/styles/components/Header.scss';
 //importando las imagenes
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import UserLogo from '../assets/static/user-icon.png';
 // const logoPlatzi = require("./src/assets/images/google-icon.png");
 // const logoUser = require('../assets/images/user-icon.png');
 //vamos a crear componentes presencionales que solo solo empujan componentes pero ni tienen logica
@@ -9,12 +11,12 @@ const Header = () => (
   <header className='header'>
     <img
       className='header__img'
-      src='/'
+      src={logo}
       alt='Platzi Video'
     />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='/' alt='user icon' />
+        <img src={UserLogo} alt='user icon' />
         <p>Perfil</p>
       </div>
       <ul>
